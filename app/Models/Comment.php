9a0b9a8b1838660e663deb5e0037ea65d539c6a7
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2025-02-03 22:01:30 +0100

    Comment model implemented

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ["user_id", "post_id", "content", "published_at"];
+    protected $casts = ['published_at' => 'datetime'];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo{
+        return $this->belongsTo(Post::class);
+    }
 }
