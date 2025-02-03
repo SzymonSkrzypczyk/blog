6dd943630c31902266ea66661c849a276f985680
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2025-02-03 21:51:18 +0100

    post model implemented

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     //
+    protected $fillable = ['user_id', 'title', 'slug', 'content', 'html_content', 'published_at'];
+    protected $casts = ['published_at' => 'datetime'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag');
+    }
+
 }
