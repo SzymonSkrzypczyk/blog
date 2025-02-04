2925a5d81955c6e353a5e05ed9abafd0e8ecdf4f
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2025-02-04 22:48:56 +0100

    policies registered

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,21 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Post;
+use App\Policies\PostPolicy;
+use App\Models\Comment;
+use App\Policies\CommentPolicy;
+use App\Models\Vote;
+use App\Policies\VotePolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Post::class => PostPolicy::class,
+        Comment::class => CommentPolicy::class,
+        Vote::class => VotePolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
