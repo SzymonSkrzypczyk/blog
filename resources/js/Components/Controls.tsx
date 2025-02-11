ba52265bfec39a47bb9233feba0622a3e616a509
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2025-02-11 18:10:55 +0100

    removed auth from Controls.tsx + removed unused pages

--- resources/js/Components/Controls.tsx
+++ resources/js/Components/Controls.tsx
@@ -1,13 +1,9 @@
-import {PageProps} from "@/types";
 
-
-export default function Controls({ auth }: PageProps) {
-    console.log(auth);
+export default function Controls() {
     return (
         <div className="fixed left-4 top-1/2 transform -translate-y-1/2 flex flex-col gap-4 align-baseline">
             <a href={route("home")}><img src="../storage/images/home-icon.svg" alt="Home" className="w-10"/></a>
             <a href="/posts"><img src="../storage/images/blog-icon.svg" alt="Blog" className="w-12"/></a>
-            {auth && auth.user && <a href="posts/create"><img src="../storage/images/write_post.svg" alt="Add Post" className="w-12"/></a>}
         </div>
     );
 }
