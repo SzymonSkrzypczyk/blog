eb5929f385385ec971557935204f16261d90ca1d
Szymon Skrzypczyk <Szymon.Skrzypczyk@vp.pl>
2025-02-20 17:18:50 +0100

    vercel.json added without secrets

--- config/cache.php
+++ config/cache.php
@@ -48,8 +48,8 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
-            'lock_path' => storage_path('framework/cache/data'),
+            'path' => "/tmp/cache",
+            //'lock_path' => storage_path('framework/cache/data'),
         ],
 
         'memcached' => [
